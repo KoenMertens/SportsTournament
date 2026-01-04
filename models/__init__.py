fd434c3fd16f61a10997aba20a8a75d9fa88e1c5
Koen Mertens <koen.mertens@hogent.be>
2026-01-04 15:14:46 +0100

    Reorder imports in __init__ to avoid circular dependency

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ Models package
 from .player import Player
 from .team import Team
 from .match import Match, MatchPhase
+# Import Tournament last to avoid circular import issues
 from .tournament import Tournament
 
 __all__ = ['Player', 'Team', 'Match', 'MatchPhase', 'Tournament']
